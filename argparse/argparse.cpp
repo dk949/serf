e5680ffe92e0cebe18224c8fbd84d74ac107dd51
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-07 22:58:30 +0300

    Added ctre

--- argparse/argparse.cpp
+++ argparse/argparse.cpp
@@ -1,7 +1,10 @@
 #include "argparse.hpp"
 
+#include<ctre.hpp>
 #include <spdlog/spdlog.h>
 
 void ap::foo() {
-    spdlog::info("argparse");
+    const auto sv = "hello";
+    const bool match = ctre::match<"h.*">(sv);
+    spdlog::info("It's{} a match",!match ? " not":"" );
 }

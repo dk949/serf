0755654800839b70d4d62ebf468338fbe9b7d30e
Davidk <56653556+dk949@users.noreply.github.com>
2021-07-07 22:21:21 +0300

    Added argparse library

---
+++ argparse/argparse.cpp
@@ -0,0 +1,7 @@
+#include "argparse.hpp"
+
+#include <spdlog/spdlog.h>
+
+void ap::foo() {
+    spdlog::info("argparse");
+}
---
+++ argparse/argparse.hpp
@@ -0,0 +1,8 @@
+#ifndef ARGPARSE_HPP
+#define ARGPARSE_HPP
+namespace ap {
+
+void foo();
+
+}  // namespace ap
+#endif // ARGPARSE_HPP

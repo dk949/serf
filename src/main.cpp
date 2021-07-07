1f937d57bc555212d38c114bc32b05b279b385db
Davidk <56653556+dk949@users.noreply.github.com>
2021-07-07 22:20:27 +0300

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,13 @@
+#include "arghandler.hpp"
+#include "gitpac.hpp"
+
 #include <iostream>
 #include <spdlog/spdlog.h>
-#include "gitpac.hpp"
-int main(){
+
+int main(int argc, const char **argv) {
     spdlog::set_pattern("[%l]: %v");
 
-    /* parse arguments here */
-
-    auto value = "https://github.com/dk949/serf_testrepo0";
-    auto difvalue = "https://github.com/dk949/serf_testrepo1";
-
-    auto maybedif = "https://github.com/dk949/serf_testrepo0";
-
-    spdlog::info("Hahing the value: {} gives hash: {}", value, std::hash<const char*>{}(value));
-    spdlog::info("Hahing the value: {} again gives hash: {}", value, std::hash<const char*>{}(value));
-
-
-    spdlog::info("Hahing another value: {} again gives hash: {}", difvalue, std::hash<const char*>{}(difvalue));
-
-    spdlog::info("Hahing the same values twice gives {} result", std::hash<const char*>{}(value) == std::hash<const char*>{}(value)? "same": "different");
-    spdlog::info("Hahing 2 similar values gives {} result", std::hash<const char*>{}(value) == std::hash<const char*>{}(difvalue)? "same": "different");
+    srf::getArgs(argc, argv);
 
     return 0;
 }

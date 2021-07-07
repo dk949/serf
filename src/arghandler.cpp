b333676a4b6547fb039b08eaf7bc3e855581c6f6
Davidk <56653556+dk949@users.noreply.github.com>
2021-07-07 22:20:52 +0300

    Added arg handler files

---
+++ src/arghandler.cpp
@@ -0,0 +1,7 @@
+#include "arghandler.hpp"
+
+#include "argparse.hpp"
+
+void srf::getArgs([[maybe_unused]] int argc, [[maybe_unused]] const char **argv) {
+    ap::foo();
+}
---
+++ src/arghandler.hpp
@@ -0,0 +1,10 @@
+#ifndef ARGHANDLER_HPP
+#define ARGHANDLER_HPP
+
+
+namespace srf{
+
+    void getArgs([[maybe_unused]] int argc, [[maybe_unused]] const char **argv);
+}
+
+#endif // ARGHANDLER_HPP

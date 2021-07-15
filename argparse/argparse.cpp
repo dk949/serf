1cdd765360e36b5028ab44637ad670797089fa6d
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-15 04:40:21 +0300

    Added basic skeleton for argparse

--- argparse/argparse.cpp
+++ argparse/argparse.cpp
@@ -1,10 +1,41 @@
 #include "argparse.hpp"
 
-#include<ctre.hpp>
 #include <spdlog/spdlog.h>
 
-void ap::foo() {
-    const auto sv = "hello";
-    const bool match = ctre::match<"h.*">(sv);
-    spdlog::info("It's{} a match",!match ? " not":"" );
+ap::Argument::Argument(const char *argName, bool optional): m_argName(argName), m_optional(optional) {}
+
+
+
+bool ap::Argument::isOptional() {
+    return m_optional;
+}
+
+const char *ap::Argument::getName() {
+    return m_argName;
+}
+
+
+
+ap::ArgumentList::ArgumentList(std::initializer_list<Argument> args): m_args(args) {
+    checkList();
+}
+
+
+constexpr void ap::ArgumentList::checkList() {
+    if (m_args[0].isOptional() || m_args[0].getName() == ap::Argument::noName) {
+        throw "Bad argument list. //TODO make this a real exception";
+    }
+}
+
+
+
+ap::ArgParse &ap::ArgParse::add(std::initializer_list<Argument> argList) {
+    m_argLists.emplace_back(argList);
+
+    return *this;
+}
+
+
+ap::ArgParse &ap::ArgParse::noArgs() {
+    return *this;
 }
--- argparse/argparse.hpp
+++ argparse/argparse.hpp
@@ -1,8 +1,69 @@
 #ifndef ARGPARSE_HPP
 #define ARGPARSE_HPP
+#include <vector>
 namespace ap {
 
-void foo();
+/*! \class Argument argparse.hpp
+ * \brief Basic building block of the argument parser.
+ *
+ * Holds information about one argument.
+ * Single command can consist of one or more `Argument`s.
+ *
+ * \see `ArgParse::noArgs()` for case with 0 arguments.
+ */
+class Argument {
+private:
+    const char *m_argName;
+    bool m_optional;
+
+public:
+    /*! \fn Argument::Argument(const char *argName, bool optional) argparse.hpp
+     * \brief Constructor. Initialises the values `m_argName` and `m_optional`.
+     *
+     * \param argName name of the argument. Can be null.
+     * \param optional whether this argument is optional on the command line.
+     *
+     * \note passing `ap::Argument::noName` for \p argName indicates that this argument can be anything
+     *  and that its value needs to be saved.
+     */
+    Argument(const char *argName, bool optional);
+
+
+    /*!
+     * \brief Variable designating that an argument has no name.
+     */
+    static constexpr const char *noName = nullptr;
+
+
+    bool isOptional();
+
+    const char *getName();
+};
+
+
+
+class ArgumentList {
+private:
+    std::vector<Argument> m_args;
+
+public:
+    ArgumentList(std::initializer_list<Argument> args);
+
+private:
+    constexpr void checkList();
+};
+
+
+
+class ArgParse {
+    std::vector<ArgumentList> m_argLists;
+
+public:
+    ArgParse() = default;
+
+    ArgParse &add(std::initializer_list<Argument>);
+    ArgParse &noArgs();
+};
 
 }  // namespace ap
-#endif // ARGPARSE_HPP
+#endif  // ARGPARSE_HPP

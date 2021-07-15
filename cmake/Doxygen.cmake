e5f8822e8c4258dd7caf5d160cad85d460881e70
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-15 03:33:25 +0300

    Added doxygen

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,11 +1,16 @@
 function(enable_doxygen)
-  option(ENABLE_DOXYGEN "Enable doxygen doc builds of source" OFF)
+    option(ENABLE_DOXYGEN "Enable doxygen doc builds of source" ON)
   if(ENABLE_DOXYGEN)
+    set(DOXYFILE_SUFFIX doxygen-docs)
+
     set(DOXYGEN_CALLER_GRAPH YES)
     set(DOXYGEN_CALL_GRAPH YES)
     set(DOXYGEN_EXTRACT_ALL YES)
+    set(DOXYGEN_JAVADOC_AUTOBRIEF YES)
+    set(DOXYGEN_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/docs")
+
     find_package(Doxygen REQUIRED dot)
-    doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR})
+    doxygen_add_docs(${DOXYFILE_SUFFIX} ${PROJECT_SOURCE_DIR})
 
   endif()
 endfunction()

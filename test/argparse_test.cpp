5627befe205df79b3e34e5ef9c256daae20c257f
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-15 21:53:49 +0300

    Added tests for operators == and !=

--- test/argparse_test.cpp
+++ test/argparse_test.cpp
@@ -31,6 +31,34 @@ TEST_CASE("ArgumentList", "[argparse][ArgumentList]") {
             ap::ArgumentList argumentList {{ap::Argument::noName, false}, {"arg1", true}, {ap::Argument::noName, false}};
         }());
     }
+
+    SECTION("getArgs") {
+        ap::ArgumentList argumentList {{"arg0", false}, {"arg1", false}, {"arg2", false}};
+        CHECK(argumentList.size() == 3);
+    }
+
+
+    SECTION("operator==") {
+        ap::ArgumentList al0 {{"arg0", false}, {"arg1", false}, {"arg2", false}};
+        ap::ArgumentList al1 {{"arg0", false}, {"arg1", false}, {"arg2", false}};
+        ap::ArgumentList al2 {{"arg0", false}, {"arg1", true}, {"arg2", true}};
+        ap::ArgumentList al3 {{"arg1", false}, {"arg1", true}, {"arg2", true}};
+
+        CHECK(al0 == al1);
+        CHECK(al0 == al2);
+        CHECK_FALSE(al0 == al3);
+    }
+
+    SECTION("operator!=") {
+        ap::ArgumentList al0 {{"arg0", false}, {"arg1", false}, {"arg2", false}};
+        ap::ArgumentList al1 {{"arg0", false}, {"arg1", false}, {"arg2", false}};
+        ap::ArgumentList al2 {{"arg0", false}, {"arg1", true}, {"arg2", true}};
+        ap::ArgumentList al3 {{"arg1", false}, {"arg1", true}, {"arg2", true}};
+
+        CHECK_FALSE(al0 != al1);
+        CHECK_FALSE(al0 != al2);
+        CHECK(al0 != al3);
+    }
 }
 
 

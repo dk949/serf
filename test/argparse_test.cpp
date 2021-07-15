fb0f1f1dbdfe63150aad62cae7f08b1e517b5412
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-15 20:19:55 +0300

    Added ArgumentList constructor tests

--- test/argparse_test.cpp
+++ test/argparse_test.cpp
@@ -2,7 +2,7 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("Argument", "[argparse][Argument][check]") {
+TEST_CASE("Argument", "[argparse][Argument]") {
     SECTION("Constructor") {
         ap::Argument argument {"testName", false};
         CHECK_THAT(argument.argName, Catch::Matchers::Equals("testName", Catch::CaseSensitive::Yes));
@@ -13,3 +13,29 @@ TEST_CASE("Argument", "[argparse][Argument][check]") {
         CHECK(ap::Argument::noName == nullptr);
     }
 }
+
+TEST_CASE("ArgumentList", "[argparse][ArgumentList]") {
+    SECTION("Constructor") {
+        // regular constructor
+        CHECK_NOTHROW([]() {
+            ap::ArgumentList argumentList {{"arg0", false}, {"arg1", true}, {ap::Argument::noName, false}};
+        }());
+
+        // constructor shoudl fail because first arg is optional
+        CHECK_THROWS([]() {
+            ap::ArgumentList argumentList {{"arg0", true}, {"arg1", true}, {ap::Argument::noName, false}};
+        }());
+
+        // constructor shoudl fail because first arg is noName
+        CHECK_THROWS([]() {
+            ap::ArgumentList argumentList {{ap::Argument::noName, false}, {"arg1", true}, {ap::Argument::noName, false}};
+        }());
+    }
+}
+
+
+// TEST_CASE("ArgParse", "[argparse][ArgParse][check]") {
+// SECTION("Constructor") {
+
+//}
+//}

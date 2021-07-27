0e2dbd95fe3f5b04ecbdcca5841c9185eb713d7b
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-27 23:48:55 +0300

    added conversion between exceptions and c errors

---
+++ lib/check.hpp
@@ -0,0 +1,13 @@
+#ifndef CHECK_HPP
+#define CHECK_HPP
+#include "git2/errors.h"
+
+#define gpCall(X)                                 \
+    if (!check(static_cast<git_error_code>((X)))) \
+    return std::nullopt
+
+namespace gp {
+bool check(git_error_code error);
+}
+
+#endif  // CHECK_HPP

bf427aba3c31448ad7e25c807af2b385cbccfc11
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-27 23:45:45 +0300

    startinmg to work on gitpac

--- lib/gitpac.hpp
+++ lib/gitpac.hpp
@@ -1,6 +1,22 @@
 #ifndef core_hpp
 #define core_hpp
-int func();
+#include "git_exception.hpp"
+#include "repo.hpp"
+
+#include <git2.h>
 
 
-#endif //core_hpp
+
+namespace gp {
+class Git {
+public:
+    // ctors and dtor
+    Git();
+    ~Git();
+};
+
+
+
+}  // namespace gp
+
+#endif  // core_hpp

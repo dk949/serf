c505350be22d6fa22b0baaffe7c04092e40d928d
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-30 23:07:58 +0300

    added representation of a package

--- lib/package.hpp
+++ lib/package.hpp
@@ -1,13 +1,17 @@
 #ifndef PACKAGE_HPP
 #define PACKAGE_HPP
 
-namespace gp{
-struct Package{
+#include "common_types.hpp"
 
-
-
+#include <optional>
+namespace gp {
+struct Package {
+    std::optional<const char *> branch;
+    std::optional<Hash_t> hash;
+    Path_t path;
+    URL_t url;
+    int urlhash;
 };
-}
+}  // namespace gp
 
-#endif // PACKAGE_HPP
-
+#endif  // PACKAGE_HPP

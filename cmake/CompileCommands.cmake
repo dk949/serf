b2c1c593737d0b9a032060d70d77cf964102422d
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-31 02:18:47 +0300

    made compile commands windows freindly

--- cmake/CompileCommands.cmake
+++ cmake/CompileCommands.cmake
@@ -1,25 +1,28 @@
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 macro (link_compile_commands)
-    set(current_list_path ${CMAKE_CURRENT_LIST_DIR})
-
-    string(
-        REGEX
-        REPLACE
-            "\/|\\|\ "
-            "_"
-            dir
-            ${current_list_path}
-        )
-
     if (CMAKE_EXPORT_COMPILE_COMMANDS)
-        add_custom_target(
-            ${dir}_compile_commands ALL
-            COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_BINARY_DIR}/compile_commands.json
-                    ${current_list_path}/compile_commands.json
-            )
-    endif ()
+        set(current_list_path ${CMAKE_CURRENT_LIST_DIR})
 
-    unset(current_list_path)
-    unset(dir)
+        string(
+            REGEX
+            REPLACE
+                "\:|\/|\\|\ "
+                "_"
+                dir
+                ${current_list_path}
+            )
+
+        if (CMAKE_EXPORT_COMPILE_COMMANDS)
+            add_custom_target(
+                ${dir}_compile_commands ALL
+                COMMAND
+                    ${CMAKE_COMMAND} -E create_symlink ${CMAKE_BINARY_DIR}/compile_commands.json
+                    ${current_list_path}/compile_commands.json
+                )
+        endif ()
+
+        unset(current_list_path)
+        unset(dir)
+    endif (CMAKE_EXPORT_COMPILE_COMMANDS)
 endmacro (link_compile_commands)

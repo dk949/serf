97c47cc6bd0136a09c2788cee15486749874245b
dk949 <56653556+dk949@users.noreply.github.com>
2021-07-31 03:11:30 +0300

    int to size_t

--- test/arghandler_test.cpp
+++ test/arghandler_test.cpp
@@ -5,13 +5,16 @@
 #include <catch2/catch.hpp>  // for SourceLineInfo, Section, operato...
 #include <memory>            // for allocator
 
+int Int(size_t s) {
+    return static_cast<int>(s);
+}
 TEST_CASE("init", "[arghandler]") {
     //### `serf init`
     //### `serf init [DIR_NAME]`
 
     SECTION("serf init (nothrow)") {  // ccls is really struggling here
         auto argv = std::array {"serf", "init"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
 
         CHECK_NOTHROW(srf::getArgs(args));
@@ -19,14 +22,14 @@ TEST_CASE("init", "[arghandler]") {
 
     SECTION("serf init dir (nothrow)") {  // ccls is really struggling here
         auto argv = std::array {"serf", "init", "dir"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf init dir arg (throws, too many args") {
         auto argv = std::array {"serf", "init", "dir", "arg"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_THROWS(srf::getArgs(args));
     }
@@ -38,28 +41,28 @@ TEST_CASE("clone", "[arghandler]") {
     //    ### `serf clone [REPO_URL]`
     SECTION("serf clone https://github.com/dk949/serf_testrepo.git (nothrow)") {
         auto argv = std::array {"serf", "clone", "https://github.com/dk949/serf_testrepo.git"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf clone git@github.com:dk949/serf_testrepo.git (nothrow)") {
         auto argv = std::array {"serf", "clone", "git@github.com:dk949/serf_testrepo.git"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf clone https://github.com/dk949/serf_testrepo.git dir (nothrow)") {
         auto argv = std::array {"serf", "clone", "https://github.com/dk949/serf_testrepo.git", "dir"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf clone git@github.com:dk949/serf_testrepo.git dir (nothrow)") {
         auto argv = std::array {"serf", "clone", "git@github.com:dk949/serf_testrepo.git", "dir"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
@@ -68,7 +71,7 @@ TEST_CASE("clone", "[arghandler]") {
     // does not throw with just 2 "arg"s because it matches the above case
     SECTION("serf clone arg arg (throws, too many args)") {
         auto argv = std::array {"serf", "clone", "arg", "arg", "arg"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_THROWS(srf::getArgs(args));
     }
@@ -78,14 +81,14 @@ TEST_CASE("delete", "[arghandler]") {
     // ### `serf delete [PATH]`
     SECTION("serf delete dir (nothrow") {
         auto argv = std::array {"serf", "delete", "dir"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf delete dir arg (throws, too many args)") {
         auto argv = std::array {"serf", "delete", "dir", "arg"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_THROWS(srf::getArgs(args));
     }
@@ -96,14 +99,14 @@ TEST_CASE("checkout", "[arghandler]") {
     // ### `serf checkout [PATH] [BRANCH/TAG/COMMIT]`
     SECTION("serf checkout dir dev (nothrow") {
         auto argv = std::array {"serf", "checkout", "dir", "dev"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf checkout dir dev arg (throws, too many args)") {
         auto argv = std::array {"serf", "checkout", "dir", "dev", "arg"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_THROWS(srf::getArgs(args));
     }
@@ -116,21 +119,21 @@ TEST_CASE("update", "[arghandler]") {
 
     SECTION("serf update (nothrow") {
         auto argv = std::array {"serf", "update"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf update dir (nothrow") {
         auto argv = std::array {"serf", "update", "dir"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf update dir arg (throws, too many args)") {
         auto argv = std::array {"serf", "update", "dir", "arg"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_THROWS(srf::getArgs(args));
     }
@@ -140,14 +143,14 @@ TEST_CASE("latest", "[arghandler]") {
     // ### `serf latest [PATH]`
     SECTION("serf latest dir (nothrow") {
         auto argv = std::array {"serf", "latest", "dir"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf latest dir arg (throws, too many args") {
         auto argv = std::array {"serf", "latest", "dir", "arg"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_THROWS(srf::getArgs(args));
     }
@@ -161,21 +164,21 @@ TEST_CASE("pull", "[arghandler]") {
 
     SECTION("serf pull (nothrow") {
         auto argv = std::array {"serf", "pull"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf pull dir (nothrow") {
         auto argv = std::array {"serf", "pull", "dir"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }
 
     SECTION("serf pull dir arg (throws, too many args)") {
         auto argv = std::array {"serf", "pull", "dir", "arg"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_THROWS(srf::getArgs(args));
     }
@@ -185,7 +188,7 @@ TEST_CASE("setup", "[arghandler]") {
     // ### `serf setup`
     SECTION("serf setup (nothrow") {
         auto argv = std::array {"serf", "setup"};
-        auto args = ap::getArgsSpan(argv.size(), argv.data());
+        auto args = ap::getArgsSpan(Int(argv.size()), argv.data());
 
         CHECK_NOTHROW(srf::getArgs(args));
     }

1b9e4c53b5717ef9ea5ec407cb3c3f45d023236b
dk949 <56653556+dk949@users.noreply.github.com>
2021-10-10 01:50:10 +0100

    bumped conan-cmake version

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,27 +1,43 @@
+set(CONAN_CMAKE_VERSION 0.16.1)
+
+if (DEFINED ENV{CONAN_USER_HOME})
+    set(CONAN_CMAKE_LOCATION $ENV{CONAN_USER_HOME}/.conan/${CONAN_CMAKE_VERSION}.conan.cmake)
+elseif (EXISTS ENV{HOME}/.conan/)
+    set(CONAN_CMAKE_LOCATION $ENV{HOME}/.conan/${CONAN_CMAKE_VERSION}.conan.cmake)
+else ()
+    set(CONAN_CMAKE_LOCATION ${CMAKE_BINARY_DIR}/${CONAN_CMAKE_VERSION}.conan.cmake)
+endif ()
+
 macro (run_conan)
     # Download automatically, you can also just copy the conan.cmake file
-    if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+    if (NOT EXISTS ${CONAN_CMAKE_LOCATION})
         message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-        file(
-            DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.16/conan.cmake"
-            "${CMAKE_BINARY_DIR}/conan.cmake"
-            )
+        file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v${CONAN_CMAKE_VERSION}/conan.cmake"
+             ${CONAN_CMAKE_LOCATION}
+        )
     endif ()
 
-    include(${CMAKE_BINARY_DIR}/conan.cmake)
+    include(${CONAN_CMAKE_LOCATION})
+    set(ENV{CONAN_REVISIONS_ENABLED} 1)
+
+    conan_add_remote(
+        NAME
+        bincrafters
+        URL
+        https://bincrafters.jfrog.io/artifactory/api/conan/public-conan
+    )
 
     conan_cmake_run(
         REQUIRES
         ${CONAN_EXTRA_REQUIRES}
-        "spdlog/1.8.5"
-        "catch2/2.13.6"
+        catch2/2.13.6
+        spdlog/1.8.5
+        ms-gsl/3.1.0
         OPTIONS
         ${CONAN_EXTRA_OPTIONS}
-        SETTINGS
-        #compiler.version=11
         BASIC_SETUP
-        CMAKE_TARGETS
+        CMAKE_TARGETS # individual targets to link to
         BUILD
         missing
-        )
+    )
 endmacro ()
